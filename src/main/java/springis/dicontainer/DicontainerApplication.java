7339383f888abc1cf7072384859b56d7f0c18b1f
남이찬 <dwbh7358@sbs.co.kr>
2025-06-25 15:09:17 +0900

    chore: initial commit

---
+++ src/main/java/springis/dicontainer/DicontainerApplication.java
@@ -0,0 +1,13 @@
+package springis.dicontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicontainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicontainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springis/dicontainer/DicontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package springis.dicontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
