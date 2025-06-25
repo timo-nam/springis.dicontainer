e16a2dafefcccb3f8a8c3e0abe5c6d812c7a7031
남이찬 <dwbh7358@sbs.co.kr>
2025-06-26 08:24:28 +0900

    chore: DI 컨테이너 노트 추가

--- src/main/java/springis/dicontainer/AppConfig.java
+++ src/main/java/springis/dicontainer/AppConfig.java
@@ -12,6 +12,7 @@ import springis.dicontainer.order.OrderServiceImpl;
 // NOTE: 애플리케이션이 구성 영역과 사용 영역으로 구분 => 관심사의 분리(SoC;Separation of Concerns)
 // NOTE: 구성 영역은 조립 방식이 명확
 // NOTE: 구성 영역에서 실행 흐름에 대한 제어 => 제어의 역전(IoC;Inversion of Control) => 프레임워크
+// NOTE: 구성을 담당하는 프레임워크 기반을 IoC 컨테이너/DI 컨테이너/어셈블러/오브젝트 팩토리라고 함
 public class AppConfig {
 
   public MemberRepository memberRepository() {
