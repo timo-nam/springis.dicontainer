7c7d87fa85f932521981d45f6f3c69aea9012667
ICHAN NAM <dwbh7358@gmail.com>
2025-06-25 23:13:32 +0900

    chore: SoC, SRP 노트 추가

--- src/main/java/springis/dicontainer/member/MemberServiceImpl.java
+++ src/main/java/springis/dicontainer/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package springis.dicontainer.member;
 
 // NOTE: 인터페이스의 구현체가 하나라면 클래스 이름을 `*Impl`로 짓는 게 관례
 // NOTE: (추상화에만 의존 => DIP 준수) -> OCP 준수
-// NOTE: 사용 영역은 로직에만 집중
+// NOTE: 사용 영역은 로직에만 집중 => 아키텍처 수준에서는 SoC, 클래스 레벨에서는 SRP 준수
 public class MemberServiceImpl implements MemberService {
 
   private final MemberRepository memberRepository;
