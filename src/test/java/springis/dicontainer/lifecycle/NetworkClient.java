53f6a5e1854cd8f84dc26b981848766155cb8856
ICHAN NAM <dwbh7358@gmail.com>
2025-06-28 08:33:18 +0900

    chore: InitializingBean, DisposableBean 사용

--- src/test/java/springis/dicontainer/lifecycle/NetworkClient.java
+++ src/test/java/springis/dicontainer/lifecycle/NetworkClient.java
@@ -1,16 +1,17 @@
 package springis.dicontainer.lifecycle;
 
 import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
 @Setter
-public class NetworkClient {
+// NOTE: InitializingBean, DisposableBean은 스프링에서 제공하는 인터페이스 => 외부 라이브러리에 적용 불가
+public class NetworkClient implements InitializingBean, DisposableBean {
 
   private String url;
 
   public NetworkClient() {
     System.out.println("생성자 호출, url = " + url);
-    connect();
-    call("초기화 연결 메세지");
   }
 
   public void connect() {
@@ -23,5 +24,16 @@ public class NetworkClient {
 
   public void disconnect() {
     System.out.println("close: " + url);
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    connect();
+    call("초기화 연결 메시지");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    disconnect();
   }
 }
