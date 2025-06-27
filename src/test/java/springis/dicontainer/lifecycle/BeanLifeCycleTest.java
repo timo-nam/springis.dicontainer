209ce2a215c0f458892054f1ab15e1374dcbb3a4
ICHAN NAM <dwbh7358@gmail.com>
2025-06-28 08:26:20 +0900

    chore: 스프링 빈의 이벤트 생명주기 이해

---
+++ src/test/java/springis/dicontainer/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,35 @@
+package springis.dicontainer.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// NOTE: <스프링 빈의 이벤트 생명주기> 컨테이너 생성 -> 빈 생성 -> 의존 관계 주입 -> 초기화 콜백 -> 사용 -> 소멸 전 콜백 -> 컨테이너 종료
+class BeanLifeCycleTest {
+
+  @Test
+  public void lifeCycleTest() {
+    // NOTE: ApplicationContext는 직접 close() 불가
+    ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(
+        LifeCycleConfig.class);
+
+    NetworkClient client = ac.getBean(NetworkClient.class);
+
+    // NOTE: close() - 스프링 컨테이너를 직접 내림
+    ac.close();
+  }
+
+  @Configuration
+  static class LifeCycleConfig {
+
+    // NOTE: 빈 생성과 초기화(무거운 동작)를 분리하는 것이 좋음
+    @Bean
+    public NetworkClient networkClient() {
+      NetworkClient networkClient = new NetworkClient();
+      networkClient.setUrl("http://watalk.sbs.co.kr");
+      return networkClient;
+    }
+  }
+}
