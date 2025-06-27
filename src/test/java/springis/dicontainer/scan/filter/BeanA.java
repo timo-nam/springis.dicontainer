c9987caf927f2aa04f6d3d1bfdc43fae6dc4ee7f
남이찬 <dwbh7358@sbs.co.kr>
2025-06-27 16:30:13 +0900

    chore: 컴포넌트 스캔 필터 사용

---
+++ src/test/java/springis/dicontainer/scan/filter/BeanA.java
@@ -0,0 +1,7 @@
+package springis.dicontainer.scan.filter;
+
+// NOTE: @Component가 없어도 포함
+@MyIncludeComponent
+public class BeanA {
+
+}
---
+++ src/test/java/springis/dicontainer/scan/filter/BeanB.java
@@ -0,0 +1,10 @@
+package springis.dicontainer.scan.filter;
+
+import org.springframework.stereotype.Component;
+
+// NOTE: @Component를 붙여도 제외
+@MyExcludeComponent
+@Component
+public class BeanB {
+
+}
